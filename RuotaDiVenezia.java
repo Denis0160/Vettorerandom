673858c86e7418ea230acaa26068b7c62b6e3cb2
Denis0160 <denis.sescu@iisviolamarchesini.edu.it>
2024-01-16 16:20:57 +0100

    Ruota di venezia

---
+++ RuotaDiVenezia.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+public class RuotaDiVenezia {
+    public static void main(String[] args) {
+
+
+        int[] vettore=new int[5];
+
+
+        for(int i=0; i<=4; i++){
+            do {
+                int casuale = valoreRandom(1, 91);
+                vettore[i] = casuale;
+
+
+                vettore[i]++;
+                System.out.println(vettore[i]);
+            }while(vettore[i]==i);
+        }
+
+
+    }
+    private static int valoreRandom(int minValue, int maxValue)
+    {
+        Random casuale = new Random(); //Metodo che non ha nessun parametro, si chiama costruttore.
+
+
+        return  casuale.nextInt(minValue,maxValue)+1;
+
+
+    }
+}
